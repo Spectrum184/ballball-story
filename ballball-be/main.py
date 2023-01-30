17fa55042671aaf1f18538410fecc25ce660b179
Thanh Nguyen <nvthanh1804@gmail.com>
2023-01-31 00:47:01 +0900

    init ballball be

---
+++ ballball-be/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.post("/hello")
+async def post_hello():
+    return {"message": "Post method"}
